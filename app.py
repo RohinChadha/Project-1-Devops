d7751d6c16ed12bc76addf06e279238867d4e1ac
rohinchadha22 <chadharohin2@gmail.com>
2026-02-04 16:39:48 +0530

    Testing GitHub Workflow Pipeline

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>This is My DevOps Project 1: Success!</h1><p>CI/CD Pipeline is running.</p>"
+def hello():
+    return "CI/CD Pipeline is Working Successfully!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
