d98613ae868eba22b10752f967f397f704861eac
rohinchadha22 <chadharohin2@gmail.com>
2026-02-04 14:00:56 +0530

    First commit: Flask app and Dockerfile

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>This is My DevOps Project 1: Success!</h1><p>CI/CD Pipeline is running.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
